e42c1929b78ed5970b7d85f62350f798cb64c353
PierreSQS <nomo_messina@web.de>
2022-02-23 00:13:45 +0100

    Chap18-use customer detail: refined SecurityConfig

--- firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
+++ firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
@@ -16,7 +16,12 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin();
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.authorizeRequests()
+				.anyRequest()
+				.authenticated()
+			.and()
+				.formLogin()
+			.and()
+				.httpBasic();
 	}
 }
