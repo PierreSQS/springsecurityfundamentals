ca92fdb0b1f94c82c627e1989ff193072287326b
PierreSQS <nomo_messina@web.de>
2022-02-25 15:05:57 +0100

    Chap18-use customer detail: alternative to previous commit(20928c00)

--- firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
+++ firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
@@ -1,27 +1,20 @@
 package com.bharath.spring.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-		UserDetails user = User.withUsername("tom").password(passwordEncoder.encode("cruise")).authorities("read").build();
+		UserDetails user = User.withUsername("tom").password("{bcrypt}$2a$10$Yi40NcoB5pl9mTGn1VJsQ.eH27E2YCUw2DiYpzk.OQPC6M0sBDFEW").authorities("read").build();
 		userDetailsService.createUser(user);
 
 		auth.userDetailsService(userDetailsService);
@@ -33,8 +26,4 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().anyRequest().authenticated();
 	}
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 }
