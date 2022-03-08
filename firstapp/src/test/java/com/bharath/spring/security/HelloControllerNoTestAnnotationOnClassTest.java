fc8ec68dad1b21e71e8e2612762b9ee3a4ec6468
PierreSQS <nomo_messina@web.de>
2022-03-08 17:07:40 +0100

    Sec3-Chap18-use customer detail: disabled test. S.the previous commit (19ea7428) and comment in the test

--- firstapp/src/test/java/com/bharath/spring/security/HelloControllerNoTestAnnotationOnClassTest.java
+++ firstapp/src/test/java/com/bharath/spring/security/HelloControllerNoTestAnnotationOnClassTest.java
@@ -1,6 +1,7 @@
 package com.bharath.spring.security;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,19 +10,26 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+// Doesn't work with these annotations, they are incomplete
+// unlike Junit4,use @WebMvcTest instead for Junit5!!
+// see the @WebMvcTest-Documentation
+@Disabled("Disabled: see the comment on top of the test")
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {MySecurityConfig.class})
 @WebAppConfiguration
-class HelloControllerNoAnnotationOnClassTest {
+class HelloControllerNoTestAnnotationOnClassTest {
 
     MockMvc mockMvc;
 
@@ -30,6 +38,7 @@ class HelloControllerNoAnnotationOnClassTest {
 
     @BeforeEach
     void setUp() {
+        mockMvc =  MockMvcBuilders.webAppContextSetup(this.wac).apply(springSecurity()).build();
     }
 
     @Test
