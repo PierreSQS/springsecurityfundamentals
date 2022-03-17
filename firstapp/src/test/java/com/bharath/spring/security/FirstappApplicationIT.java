a893d7929eba3ded21ea5dc5ac9d324fb08026f3
PierreSQS <nomo_messina@web.de>
2022-03-17 12:23:09 +0100

    Sec3-Chap21_22-use customer detail:renamed the IT-tests accordingly, change the FailSafe-version

--- firstapp/src/test/java/com/bharath/spring/security/FirstappApplicationIT.java
+++ firstapp/src/test/java/com/bharath/spring/security/FirstappApplicationIT.java
@@ -4,15 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
-@SpringBootTest
-class FirstappApplicationTests {
+class FirstappApplicationIT {
 
 	@Test
 	void testPasswordEncoders() {
