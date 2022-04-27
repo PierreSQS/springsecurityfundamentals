24bfadd34e6ff126dff98a63d2a684d95b7e05d7
PierreSQS <nomo_messina@web.de>
2022-04-27 17:01:52 +0200

    Sec3-Chap25-create custom filter: added the associated code and debug entry in properties file

--- firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
+++ firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+//@EnableWebSecurity(debug = true)
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -20,8 +23,9 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic();
+		http.httpBasic().and().formLogin();
 		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilterBefore(new MySecurityFilter(), BasicAuthenticationFilter.class);
 	}
 
 }
---
+++ firstapp/src/main/java/com/bharath/spring/security/MySecurityFilter.java
@@ -0,0 +1,13 @@
+package com.bharath.spring.security;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MySecurityFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Before");
+        chain.doFilter(request,response);
+        System.out.println("After");
+    }
+}
