25529c97a9015b43a69e3295f76ac10933b36835
PierreSQS <nomo_messina@web.de>
2022-04-28 12:10:22 +0200

    Sec4-Chap30_35: initial commit CouponService

--- couponservice/sql/tables.sql
+++ couponservice/sql/tables.sql
@@ -1,13 +1,13 @@
-use mydb;
+USE bharhatspringsecuritydb;
 
-create table product(
+CREATE TABLE product(
 id int AUTO_INCREMENT PRIMARY KEY,
 name varchar(20),
 description varchar(100),
 price decimal(8,3) 
 );
 
-create table coupon(
+CREATE TABLE coupon(
 id int AUTO_INCREMENT PRIMARY KEY,
 code varchar(20) UNIQUE,
 discount decimal(8,3),
