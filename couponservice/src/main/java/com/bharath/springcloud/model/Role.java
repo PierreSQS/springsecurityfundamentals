1c14c525bbbfcb9ef05b048cd2f7d4fcaa41f935
PierreSQS <nomo_messina@web.de>
2022-04-29 17:28:57 +0200

    Sec5-Chap46-Create Entities: created Security Entities for CouponService

---
+++ couponservice/src/main/java/com/bharath/springcloud/model/Role.java
@@ -0,0 +1,31 @@
+package com.bharath.springcloud.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
