b81da97158018112ac4a30d3747a84e22c290c37
PierreSQS <nomo_messina@web.de>
2022-04-30 10:20:00 +0200

    Sec6-Chap57-Create Index Page: secured the index path "/" and adapted the test

--- couponservice/src/test/java/com/bharath/springcloud/controllers/CouponControllerTest.java
+++ couponservice/src/test/java/com/bharath/springcloud/controllers/CouponControllerTest.java
@@ -3,10 +3,10 @@ package com.bharath.springcloud.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -18,6 +18,7 @@ class CouponControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @WithMockUser
     @Test
     void index() throws Exception {
         mockMvc.perform(get("/"))
