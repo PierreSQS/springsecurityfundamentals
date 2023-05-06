6f23cb94eb39fc67fdd63ff846a05050f8b9d451
PierreSQS <nomo_messina@web.de>
2023-05-07 00:06:05 +0200

    ASSN1-Create µServices: related code

--- productservice/src/main/java/com/bharath/springcloud/controllers/ProductRestController.java
+++ productservice/src/main/java/com/bharath/springcloud/controllers/ProductRestController.java
@@ -1,33 +1,40 @@
 package com.bharath.springcloud.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
 import com.bharath.springcloud.dto.Coupon;
 import com.bharath.springcloud.model.Product;
 import com.bharath.springcloud.repos.ProductRepo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/productapi")
 public class ProductRestController {
 
-	@Autowired
-	private ProductRepo repo;
+	private final ProductRepo productRepo;
 
-	@Autowired
-	private RestTemplate restTemplate;
+	private final RestTemplate restTemplate;
 
 	@Value("${couponService.url}")
 	private String couponServiceURL;
 
-	@PostMapping(value = "/products")
+	public ProductRestController(ProductRepo productRepo, RestTemplate restTemplate) {
+		this.productRepo = productRepo;
+		this.restTemplate = restTemplate;
+	}
+
+	@PostMapping("/products")
 	public Product create(@RequestBody Product product) {
 		Coupon coupon = restTemplate.getForObject(couponServiceURL + product.getCouponCode(), Coupon.class);
 		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
-		return repo.save(product);
+		return productRepo.save(product);
 
 	}
+
+	@GetMapping("/products/{productID}")
+	public Product getProduct(@PathVariable Long productID) {
+		return productRepo.findById(productID).orElse(null);
+	}
+
 
 }
