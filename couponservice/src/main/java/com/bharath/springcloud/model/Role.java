da53be74c72b56522b4ed8080302bdc5af699151
PierreSQS <nomo_messina@web.de>
2023-05-07 09:25:33 +0200

    Sect5_Chap43-Create Entities: related code

---
+++ couponservice/src/main/java/com/bharath/springcloud/model/Role.java
@@ -0,0 +1,32 @@
+package com.bharath.springcloud.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.roleName;
+    }
+}
