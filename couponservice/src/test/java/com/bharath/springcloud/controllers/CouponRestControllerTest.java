5560a7fa96c70283e3a112f25737f9a24d2871b7
PierreSQS <nomo_messina@web.de>
2023-05-07 16:18:31 +0200

    Sect5_Chap47-Configure Security: added a new test

--- couponservice/src/test/java/com/bharath/springcloud/controllers/CouponRestControllerTest.java
+++ couponservice/src/test/java/com/bharath/springcloud/controllers/CouponRestControllerTest.java
@@ -1,21 +1,37 @@
 package com.bharath.springcloud.controllers;
 
 import com.bharath.springcloud.config.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CouponRestControllerTest.class)
 @Import(SecurityConfig.class)
 class CouponRestControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
-    void create() {
+    void create() throws Exception {
+        mockMvc.perform(post("/couponapi/coupons")
+                        .with(httpBasic("doug@bailey.com","doug")))
+                .andExpect(status().isOk());
     }
 
     @Test
