1370cad392a8b1ba97adfdea213412a47b785981
PierreSQS <nomo_messina@web.de>
2023-05-07 14:40:41 +0200

    Sect5_Chap47-Configure Security: related code

--- couponservice/src/main/java/com/bharath/springcloud/config/SecurityConfig.java
+++ couponservice/src/main/java/com/bharath/springcloud/config/SecurityConfig.java
@@ -1,9 +1,27 @@
 package com.bharath.springcloud.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic()
+        .and()
+                .authorizeHttpRequests()
+                    .requestMatchers(HttpMethod.GET,"/couponapi/coupons/**")
+                    .hasAnyRole("ADMIN","USER")
+                    .requestMatchers(HttpMethod.POST,"/couponapi/coupons").hasRole("ADMIN")
+        .and()
+                .csrf().disable();
+
+        return httpSecurity.build();
+    }
 
 }
