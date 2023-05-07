b5db367dac2f7d3b546dab76b0e8b7038f98152f
PierreSQS <nomo_messina@web.de>
2023-05-07 16:57:32 +0200

    Sect5_Chap47-Configure Security-NOK: added a new test

--- couponservice/src/test/java/com/bharath/springcloud/controllers/CouponRestControllerTest.java
+++ couponservice/src/test/java/com/bharath/springcloud/controllers/CouponRestControllerTest.java
@@ -1,36 +1,55 @@
 package com.bharath.springcloud.controllers;
 
 import com.bharath.springcloud.config.SecurityConfig;
+import com.bharath.springcloud.model.Coupon;
+import com.bharath.springcloud.repos.CouponRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import java.math.BigDecimal;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(CouponRestControllerTest.class)
-@Import(SecurityConfig.class)
+@WebMvcTest(controllers = {CouponRestControllerTest.class})
+@Import({SecurityConfig.class})
 class CouponRestControllerTest {
+
+    private Coupon coupon;
 
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    CouponRepo couponRepoMock;
+
     @Autowired
     ObjectMapper objectMapper;
+    @Autowired
+    private CouponRepo couponRepo;
 
     @BeforeEach
     void setUp() {
+        coupon = new Coupon();
+        coupon.setCode("SUPERSALE");
+        coupon.setDiscount(BigDecimal.TEN);
+        coupon.setExpDate("12/12/2023");
     }
 
     @Test
     void create() throws Exception {
         mockMvc.perform(post("/couponapi/coupons")
-                        .with(httpBasic("doug@bailey.com","doug")))
+                        .with(user("mock@bailey.com").password("pwd").roles("ADMIN"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(coupon)))
                 .andExpect(status().isOk());
     }
 
