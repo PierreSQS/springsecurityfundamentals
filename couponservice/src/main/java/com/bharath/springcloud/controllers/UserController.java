5c3cd266db03640905231b5aaa972ac1c18ab414
PierreSQS <nomo_messina@web.de>
2023-05-13 13:19:06 +0200

    Sect6_Chap61-Implement User Registration: made the TDD registerUser()-Test work

--- couponservice/src/main/java/com/bharath/springcloud/controllers/UserController.java
+++ couponservice/src/main/java/com/bharath/springcloud/controllers/UserController.java
@@ -1,12 +1,40 @@
 package com.bharath.springcloud.controllers;
 
+import com.bharath.springcloud.model.Role;
+import com.bharath.springcloud.model.User;
+import com.bharath.springcloud.repos.UserRepo;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Set;
 
 @Controller
 public class UserController {
+    private final UserRepo userRepo;
+
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @GetMapping("/showReg")
     public String showRegistrationForm() {
         return "registerUser";
+    }
+
+    @PostMapping("/registerUser")
+    public String registerUser(User user) {
+        Role userRole = new Role();
+        userRole.setRoleName("ROLE_USER");
+        user.setRoles(Set.of(userRole));
+        user.setPassword(encodePWD(user.getPassword()));
+
+        userRepo.save(user);
+        return "redirect:/login";
+    }
+
+    private String encodePWD(String pwd) {
+        return "{bcrypt}"+new BCryptPasswordEncoder().encode(pwd);
     }
 }
