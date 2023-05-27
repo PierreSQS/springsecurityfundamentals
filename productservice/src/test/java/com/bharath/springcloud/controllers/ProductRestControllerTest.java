7c3e96495d9db6b3881b3f23a239e35f9ce06da6
PierreSQS <nomo_messina@web.de>
2023-05-27 23:11:59 +0200

    Sect5_ASSN2-Secure ProductServ. REST APIs: added a Test

---
+++ productservice/src/test/java/com/bharath/springcloud/controllers/ProductRestControllerTest.java
@@ -0,0 +1,60 @@
+package com.bharath.springcloud.controllers;
+
+import com.bharath.springcloud.model.Product;
+import com.bharath.springcloud.repos.ProductRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductRestController.class)
+class ProductRestControllerTest {
+
+    private Product product1;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ProductRepo productRepoMock;
+
+    @BeforeEach
+    void setUp() {
+        product1 = new Product();
+        product1.setPrice(BigDecimal.TEN);
+        product1.setName("USB-Storage");
+        product1.setDescription("Storage Device");
+        product1.setCouponCode("SUPERSALE");
+    }
+
+    @Test
+    void getProductByID() throws Exception {
+        // given
+        Product savedProduct = new Product();
+        savedProduct.setId(1L);
+        savedProduct.setCouponCode(product1.getCouponCode());
+        savedProduct.setName(product1.getName());
+        savedProduct.setPrice(product1.getPrice());
+        savedProduct.setDescription(product1.getDescription());
+        given(productRepoMock.findById(anyLong())).willReturn(Optional.of(savedProduct));
+
+        // when then
+        mockMvc.perform(get("/productapi/products/{productID}",1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(equalTo(1)))
+                .andExpect(jsonPath("$.name").value(equalTo("USB-Storage")))
+                .andDo(print());
+    }
+}
