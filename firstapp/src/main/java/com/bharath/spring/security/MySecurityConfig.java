c3e7e461adca40f98ab3f9bce5558e3eff56c19c
PierreSQS <nomo_messina@web.de>
2023-05-28 08:15:12 +0200

    Sect4_Chap18_TO_Chap21-Miscs.: migrated code to SB3.1.0

--- firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
+++ firstapp/src/main/java/com/bharath/spring/security/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.bharath.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * New Configuration Style since 3.1.0 !!!!
+ */
 @Configuration
 public class MySecurityConfig  {
 
@@ -24,11 +28,9 @@ public class MySecurityConfig  {
 
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-				.anyRequest()
-				.authenticated()
-			.and()
-				.formLogin();
+		http
+			.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+				.formLogin(Customizer.withDefaults());
 
 		return http.build();
 	}
